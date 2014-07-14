e6712538164a60aaf1387ae66458408524003a56
mastermindg <mastermindg@gmail.com>
2014-07-14 12:58:42 -0400

    Update scan.js

--- www/scripts/scan.js
+++ www/scripts/scan.js
@@ -28,17 +28,17 @@ function processBarcode(barcode) {
 	var barcodes="0049000006582";
 	var url="http://eandata.com/feed/?v=3&keycode=" + eankey + "&mode=json&find=";
 	$.getJSON(url + barcodes, null, function(results) {
-	//processBarcode(results);
-	console.log(results);	
-	var status = results['status'];	
-	// Check that we got valid results	
-	if(status['code'] == 200) {
-		//$('#targetDiv').append('OK');
-		var product = results['product'];
-		var desc = product['attributes']['product'];
-		return desc;
-	} else {
-		return "Invalid barcode";
-	}
-});	
+		//processBarcode(results);
+		console.log(results);	
+		var status = results['status'];	
+		// Check that we got valid results	
+		if(status['code'] == 200) {
+			//$('#targetDiv').append('OK');
+			var product = results['product'];
+			var desc = product['attributes']['product'];
+			return desc;
+		} else {
+			return "Invalid barcode";
+		}
+        });	
 }
